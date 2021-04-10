cf620582c5301431aa57adf38a6225f2b502d8de
akanksha.mahajan <akankshamahajan99@gmail.com>
2021-04-10 09:50:31 +0530

    All Schemas

--- contracts/EDUContract.sol
+++ contracts/EDUContract.sol
@@ -18,7 +18,95 @@ contract EDUContract {
         string email;
         uint256[] document;
     }
+    struct CollegeRequest {
+        uint256 index;
+        address student;
+        uint256 datetime;
+        RequestStat stat;
+        string remark;
+        address college;
+        string docpath;
+    }
 
+    struct OrganizationRequest {
+        uint256 index;
+        address student;
+        uint256 datetime;
+        RequestStat stat;
+        string remark;
+        address organization;
+        string docpath;
+    }
+
+    struct College {
+        address Id;
+        string collegename;
+        string clgaddress;
+        string email;
+    }
+
+    struct Organization {
+        address Id;
+        string orgname;
+        string orgaddress;
+        string email;
+    }
+
+    struct Document {
+        uint256 index;
+        address institute;
+        address student;
+        DocSender sender;
+        string docname;
+        string docpath;
+        string docdesc;
+        bool isActive;
+    }
+
+    struct CollegeAdmin {
+        address Id;
+        string collegeadminname;
+        string clgadminaddress;
+        string email;
+    }
+
+    struct OrganizationAdmin {
+        address Id;
+        string orgadminname;
+        string orgadminaddress;
+        string email;
+    }
+
+    mapping(address => Student) public students;
+    mapping(address => College) public colleges;
+    mapping(address => Organization) public organizations;
+    mapping(address => CollegeAdmin) public collegeAdministrator;
+    mapping(address => OrganizationAdmin) public organizationAdministartor;
+    mapping(uint256 => CollegeRequest) public collegeRequests;
+    mapping(uint256 => OrganizationRequest) public organizationRequests;
+    mapping(address => bool) public administrator;
+    mapping(address => bool) public isCollege;
+    mapping(address => bool) public isStudent;
+    mapping(address => bool) public isOrganization;
+    mapping(address => bool) public isCollegeAdministartor;
+    mapping(address => bool) public isOrganizationAdministrator;
+    mapping(uint256 => Document) public documents;
+
+    uint256 public CollegeRequestIndex;
+    uint256 public OrganizationRequestIndex;
+    uint256 public DocumentIndex;
+
+    address public owner;
+
+    string public version = "0.0.1";
+
+    event eCollegeRequestAdd(uint256 indexed index, address sender);
+
+    event eCollegeRequestUpdate(uint256 indexed index, address sender);
+    
+    event eOrganizationRequestAdd(uint256 indexed index, address sender);
+
+    event eOrganizationRequestUpdate(uint256 indexed index, address sender);
 
 
 }
